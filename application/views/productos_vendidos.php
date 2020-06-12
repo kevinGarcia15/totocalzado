ec8ab47a8ed6bda5e3a8f05f1a0dbe844f633433
Kevin Garcia <kgarciac8@miumg.edu.gt>
2020-06-12 09:39:10 -0600

    Redimencionado tabla de productos vendidos

--- application/views/productos_vendidos.php
+++ application/views/productos_vendidos.php
@@ -8,28 +8,26 @@ $totalDia = 0;
 	<?php $this->load->view('header'); ?>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.css" id="theme-styles">
-	<link rel="stylesheet" href="<?=$base_url?>recursos/css/productos_vendidos.css">
+	<link rel="stylesheet" href="<?=$base_url?>/recursos/css/productos_vendidos.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<title>productos vendidos</title>
 </head>
 <body>
 	<header>
-	</header>
-	<div class="page-content p-5" id="content">
-		<br>
 		<?php $this->load->view('menu'); ?>
-
+	</header>
+	<div class="page-content" id="content">
 	<div class="col-xs-12">
-		<h1>Ventas realizadas</h1>
+		<h2 class="font-color-white">Ventas realizadas</h2>
 		<div>
-			<a class="btn btn-success" href="./nuevaVenta">Nueva <i class="fa fa-plus"></i></a>
+			<a class="btn btn-success" href="./nuevaVenta">Nueva venta<i class="fa fa-plus"></i></a>
 		</div>
 		<br>
 			<div class="table-responsive-xl">
 				<table class="table table-bordered table-color">
-				<thead>
+				<thead class="font-color-white">
 					<tr>
-						<th><div class="sizeTable">Fecha</div></th>
+						<th><div class="headTable-L">Fecha</div></th>
 						<th>Productos vendidos</th>
 					</tr>
 				</thead>
@@ -39,19 +37,19 @@ $totalDia = 0;
 								<select class="custom-select" onchange="mostrarProductos()" name="fecha" id="fechasSelect">
 										<option value="">seleccionar</option>
 								</select>
-								<div id="fechaNombre"></div>
+								<div id="fechaNombre" class="font-color-white"></div>
 						</td>
 						<td>
 							<table class="table table-bordered table-color">
 								<thead>
 									<tr>
-										<th><div class="sizeTable">Código</div></th>
-										<th><div class="sizeTable">Número</div></th>
-										<th><div class="sizeTableXL">Descripción</div></th>
-										<th><div class="sizeTable">Costo C/U</div></th>
+										<th><div class="headTable-m">Código</div></th>
+										<th><div>Número</div></th>
+										<th><div class="headTable-XL">Descripción</div></th>
+										<th><div class="headTable-m">Costo C/U</div></th>
 										<th>Cantidad</th>
-										<th><div class="sizeTable">Sub-total</div></th>
-										<th><div class="sizeTable">Total</div></th>
+										<th><div class="headTable-m">Sub-total</div></th>
+										<th><div class="headTable-m">Total</div></th>
 									</tr>
 								</thead>
 								<tbody id="producto">
@@ -66,8 +64,8 @@ $totalDia = 0;
 			</table>
 		</div>
 	</div>
-	  <?php $this->load->view('footer'); ?>
 </div>
+<?php $this->load->view('footer'); ?>
 <script type="text/javascript">
 //muestra antes de cargar la pàgina
 $(window).load(function() {
